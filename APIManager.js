81fbc00e1e5121858409ee0a1d698749aef6187e
Abigail Einhorn <abbyeinhorn@gmail.com>
2018-12-06 22:05:22 +0200

    finished API

--- APIManager.js
+++ APIManager.js
@@ -2,22 +2,64 @@
 //Each method should also invoke the renderer for its own section
 
 class APIManager {
-    constructor() {
+    constructor(renderPage) {
+        this.renderPage = renderPage
 
     }
     getUsers() {
-        // Generate 7 new Users
+        $.ajax({
+            method: "GET",
+            url: 'https://randomuser.me/api/1.2/?results=7',
+            dataType: 'json',
+            success: (data) => {
+                let randomUser = data.results[0]
+                let randomFriends = data.results.splice(1)
+                console.log(randomUser)
+                console.log(randomFriends)
+                this.renderPage.renderUsers(randomUser)
+                this.renderPage.renderFriends(randomFriends)
+            }
+        });
     }
     getQuote() {
-        // get a random quote and author
+        $.ajax({
+            method: "GET",
+            url: 'https://talaikis.com/api/quotes/random/',
+            dataType: 'json',
+            success: (data) => {
+                this.renderPage.renderQuote(data)
+            }
+        });
     }
     getPokemon() {
-        // Generate a random pokemon
+
+        let randomNum = Math.floor(Math.random() * 150) + 1
+        $.ajax({
+            method: "GET",
+            url: `https://pokeapi.co/api/v2/pokemon/${randomNum}/`,
+            success: (data) => {
+
+                this.renderPage.renderPokemon(data)
+            }
+        });
     }
     getMeat() {
-        // generate two paragraphs of text that are all meat
+        $.ajax({
+            method: "GET",
+            url: 'https://baconipsum.com/api/?type=all-meat&paras=2&start-with-lorem=1',
+            dataType: 'json',
+            success: (data) => {
+                console.log(data)
+                this.renderPage.renderMeat(data)
+
+            }
+        });
     }
     generateNewPage() {
-        // generate your new page here
+        this.getUsers();
+        this.getQuote();
+        this.getPokemon();
+        this.getMeat()
     }
 }
+
--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 // Create instances of your classes here
 
-
-
-
+let renderPage = new Renderer
 
 
 // Render your page and create an on-click to generate a new user here
 
+const x = new APIManager (renderPage)
+x.getUsers()
+x.getQuote()
+x.getPokemon()
+x.getMeat()
+
+
+$("#gen-page").on("click", function(){
+    x.generateNewPage()
+    // $(".container").empty()
+})
